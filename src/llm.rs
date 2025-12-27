99f9d6fec27d0731bf65d8ea71653812d23b5b53
Justin Wiblin <justinw@cosiestdevil.uk>
2025-12-27 22:07:02 +0000

    feat(llm): make LLMMode serializable and add menu conversion helpers

--- src/llm.rs
+++ src/llm.rs
@@ -1,6 +1,6 @@
 use clap::ValueEnum;
 pub use llm::*;
-#[derive(Clone, Copy, ValueEnum, Debug)]
+#[derive(Clone, Copy, ValueEnum, Debug,serde::Serialize, serde::Deserialize)]
 pub enum LLMMode {
     None = 0,
     Cpu = 1,
@@ -21,6 +21,24 @@ impl From<LLMMode> for llm::LLMMode {
             LLMMode::None => llm::LLMMode::None,
             LLMMode::Cpu => llm::LLMMode::Cpu,
             LLMMode::Cuda => llm::LLMMode::Cuda,
+        }
+    }
+}
+impl From<LLMMode> for menu::LLMMode {
+    fn from(value: LLMMode) -> Self {
+        match value {
+            LLMMode::None => menu::LLMMode::None,
+            LLMMode::Cpu => menu::LLMMode::Cpu,
+            LLMMode::Cuda => menu::LLMMode::Cuda,
+        }
+    }
+}
+impl From<menu::LLMMode> for LLMMode {
+    fn from(value: menu::LLMMode) -> Self {
+        match value {
+            menu::LLMMode::None => LLMMode::None,
+            menu::LLMMode::Cpu => LLMMode::Cpu,
+            menu::LLMMode::Cuda => LLMMode::Cuda,
         }
     }
 }
